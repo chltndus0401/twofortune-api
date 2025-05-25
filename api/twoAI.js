47cea515cc80e3e9db02e215375bc64da8d07354
chltndus0401 <ally5336@duksung.ac.kr>
2025-05-25 11:16:11 +0900

    re

--- api/twoAI.js
+++ api/twoAI.js
@@ -5,12 +5,9 @@ dotenv.config();
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export default async function handler(req, res) {
-  // CORS 헤더 (항상 설정)
-  res.setHeader("Access-Control-Allow-Origin", "https://chltndus0401.github.io");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Vary", "Origin");
-
   // 프리플라이트 요청 처리
   if (req.method === "OPTIONS") {
     return res.status(200).end();
